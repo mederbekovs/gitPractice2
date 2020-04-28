ae4a16d52739b59f2d108e99537d705c9fd305d6
mederbekovs <64219751+mederbekovs@users.noreply.github.com>
2020-04-28 12:52:05 -0700

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -1,4 +1,11 @@
 package day_1;
 
 public class HappyBirthday {
+  
+  
+  public static void main(String[] args){
+    System.out.println("Happy birthday B18");
+    
+    System.out.println("Happy Birthday again, this line was wrote from GitHub");
+  }
 }
