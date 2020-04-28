1fe4fadb4a005487d52d6b499dd4387a815fe5d6
mederbekovs <mederbekovs01@gmail.com>
2020-04-28 12:26:32 -0700

    add addCommand.java

---
+++ src/day_1/addCommand.java
@@ -0,0 +1,14 @@
+package day_1;
+
+public class addCommand {
+
+    public static void main(String[] args) {
+
+
+
+
+        // I want to try git add filename
+
+        // I made chaned here when I created addCommand class
+    }
+}
